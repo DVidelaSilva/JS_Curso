7e359ae84f49a80c0669327ac3565c2c250e4ba4
DAVS <dvidelasilva@gmail.com>
2024-01-04 17:07:19 -0300

    primer commit

---
+++ repaso.js
@@ -0,0 +1,12 @@
+let usuario = prompt("Ingrese el usuario");
+
+
+usuario = usuario.toLowerCase();
+
+console.log(usuario);
+
+if (usuario == 'john' ) {
+    alert("Bienvenido");
+} else {
+    alert("No te conocemos");
+}
