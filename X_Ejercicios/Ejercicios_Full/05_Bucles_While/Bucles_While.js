fd9f1f282136ef51321c5f74b0f7333c938dad32
DAVS <dvidelasilva@gmail.com>
2024-01-16 21:45:57 -0300

    Ejercicios full 5 y 6

--- X_Ejercicios/Ejercicios_Full/05_Bucles_While/Bucles_While.js
+++ X_Ejercicios/Ejercicios_Full/05_Bucles_While/Bucles_While.js
@@ -111,6 +111,7 @@ alert(suma);
 
 
 
+
 /* 
 // 7 - Numero mayor y menor ingresado
 
@@ -133,4 +134,84 @@ alert("El numero mayor ingresado es: " + max + " El numero menor ingresado es: "
 
 
 
-// 8 - 
+
+/*
+ // 8 - Solo numeros pares
+
+let numero = parseInt(prompt("Ingrese solo numeros pares: "))
+let contador = 0;
+let par = numero % 2;
+
+while(par == 0){
+    
+    contador++;
+    numero = parseInt(prompt("Ingrese solo numeros pares: "))
+    par = numero % 2;
+
+};
+
+alert("Has escrito " + contador + " numeros Pares"); 
+*/
+
+
+
+/* 
+// 9 - selecciona una opcion
+
+
+let opcion = prompt("Menu de Opciones: " + "\n\n 1 -->  Leer datos" + "\n 2 -->  Mostrar datos" + "\n 3 -->  Fin" + "\n\n Selecciona una Opcion");
+
+while( opcion != "3"){
+
+    switch(opcion){
+        case "1":
+            alert("Has seleccionado la opcion " + opcion);
+            break;
+        case "2": 
+            alert("Has seleccionado la opcion " + opcion);
+            break;
+        default:
+            alert("ERROR! opcion invalida");
+    }       
+    opcion = prompt("Menu de Opciones: " + "\n\n 1 -->  Leer datos" + "\n 2 -->  Mostrar datos" + "\n 3 -->  Fin" + "\n\n Selecciona una Opcion");
+};
+
+alert("Fin del Programa");
+*/
+
+
+
+/* 
+// 10 - pedir numeros, sumar y si resultado es par repetir
+
+let numero = 0;
+let suma = 0;
+
+
+while((suma % 2) == 0){
+    numero = parseInt(prompt("Ingrese un numero: "));
+    while(numero != 0){
+        suma += numero;
+        
+        numero = parseInt(prompt("Ingrese un numero: "));
+    }
+
+    alert(suma); 
+
+    
+} 
+    alert("FIN") 
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
