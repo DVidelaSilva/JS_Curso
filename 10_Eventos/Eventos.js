db445ba780f4b08477565dc9d88aa52cfa1e47e1
DAVS <dvidelasilva@gmail.com>
2024-01-22 15:56:05 -0300

    10 eventos update

--- 10_Eventos/Eventos.js
+++ 10_Eventos/Eventos.js
@@ -4,6 +4,8 @@ const inputNombre = document.querySelector('#nombre');
 const btn2 = document.querySelector('#btn-2');
 const btn3 = document.querySelector('#btn-3');
 
+const p = document.querySelector('p');
+
 // formulario
 
 const inputEmail = document.querySelector('#inputEmail');
@@ -42,20 +44,20 @@ function login(){
 
 // opcion 1 - con esta forma si se repiten los eventos se escucha solo el ultimo evento
 
-btn2.onclick = () => {
+/* btn2.onclick = () => {
     alert('EH, porque haces click?!');
 }
 
 btn2.onclick = () => {
     alert('EH, porque haces click? otra vez!');
 }
-
+ */
 
 // opcion 2 agrego un escuchador de eventos - con esta forma si se repiten los eventos se escuchan todos los eventos
 
-btn3.addEventListener('click', function(){
+/* btn3.addEventListener('click', function(){
     console.log('Se hizo click desde el btn III')
-})
+}) */
 
 /* btn3.addEventListener('click', function(){
     console.log('Se hizo click otra vez desde el btn III')
@@ -63,16 +65,16 @@ btn3.addEventListener('click', function(){
  */
 
 
-inputNombre.addEventListener('change', () => {
+/* inputNombre.addEventListener('change', () => {
     console.log(inputNombre.value);
 })
-
+ */
 
 
 // formulario
 
 
-function validarEmailPass(){
+/* function validarEmailPass(){
     const email = inputEmail.value;
     const pass = inputPassword.value;
     
@@ -83,4 +85,26 @@ formulario.addEventListener('submit', (evento) => {
     evento.preventDefault(); // evito que el formulario recarge la pagina
     console.log(evento)
     validarEmailPass();
+})
+ */
+
+inputNombre.addEventListener('blur', () =>{
+    const nombre = inputNombre.value.trim(); 
+
+    if (nombre.length < 3){
+       p.innerText = 'El nombre debe contener mas de 3 catacteres';
+    }else{
+        p.innerText = '';
+    }
+
+    console.log(inputNombre.value);
+})
+
+inputNombre.addEventListener('input', () => {
+    const nombre = inputNombre.validarEmailPass;
+    p.innerText = nombre;
+})
+
+inputNombre.addEventListener('focus', () => {
+    p.innerText = ''
 })
